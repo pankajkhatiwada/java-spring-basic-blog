70a92de4da4c7800baa1ce6d122aaa34d3be8dd2
pankajkhatiwada <pankajkhatiwada@gmail.com>
2020-04-25 09:46:43 +0545

    Get Title

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -19,9 +19,8 @@ public class BlogController {
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
-        List<Post> allPosts = postRepository.getAllPosts();
-        modelMap.put("title","Blog Post 1");
-        modelMap.put("posts", "allPosts");
+        List<Post> posts = postRepository.getAllPosts();
+        modelMap.put("posts",posts );
         return "home";
     }
 
